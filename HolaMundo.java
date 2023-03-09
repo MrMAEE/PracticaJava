30e3fa24c50745cfae5a3e9fd047f755947b8ef0
MrMAEE <LE22080093@merida.tecnm.mx>
2023-03-09 13:30:44 -0600

    test Hola mundo

---
+++ HolaMundo.java
@@ -0,0 +1,12 @@
+public class HolaMundo
+{
+    public static void main (String[] xd)
+    {
+        //Crea una variable para guardar el texto
+        String a="Adios Mundo";
+        //Imprime la variable
+        System.out.println(a);
+        //Finaliza el programa
+        return;
+    }
+}
